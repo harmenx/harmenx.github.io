238eb0b2e1647a83a5ec2c3d46c7c1a5ac98cece
Harmen <harmendark@gmail.com>
2019-12-17 23:47:24 +0000

    Rework

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,5 @@
-$(function () {
+window.onload = function () {
 
-
-  // EVENT HANDLERS
   $('.page-link').click(function() {
     var anchor = $(this).attr("dest");
     $('.link-wrap').removeClass('visible');
@@ -22,58 +20,58 @@ $(function () {
     $('#success').removeClass('expand');
   })
 
-});
-
-window.onload = function () {
-  
-  $("#contact-button").click(function (event) {
-
-    event.preventDefault();
-    $('form').fadeOut(500);
-    $('.contactWrapper').addClass('form-success');
-    $('.contactWrapper h1').html('Thank you!');
-
-    $.ajax({
-      url: "https://formcarry.com/s/VNlNBJYI8hO",
-      method: "POST",
-      data: { message: $('form').serialize() },
-      dataType: "json"
-    }).done(function (response) {
-      $('#success').addClass('expand');
-      $('#contact-form').find("input[type=text], input[type=email], textarea").val("");
-    });
-  });
 };
 
+// 
+  
+//   $("#contact-button").click(function (event) {
 
-$(window).on('scroll', function () {
+//     event.preventDefault();
+//     $('form').fadeOut(500);
+//     $('.contactWrapper').addClass('form-success');
+//     $('.contactWrapper h1').html('Thank you!');
 
-  var pos = $(window).scrollTop();
-  var pos2 = pos + 50;
-  var scrollBottom = pos + $(window).height();
+//     $.ajax({
+//       url: "https://formcarry.com/s/VNlNBJYI8hO",
+//       method: "POST",
+//       data: { message: $('form').serialize() },
+//       dataType: "json"
+//     }).done(function (response) {
+//       $('#success').addClass('expand');
+//       $('#contact-form').find("input[type=text], input[type=email], textarea").val("");
+//     });
+//   });
+// };
+
+
+// $(window).on('scroll', function () {
+
+//   var pos = $(window).scrollTop();
+//   var pos2 = pos + 50;
+//   var scrollBottom = pos + $(window).height();
 
 
 
-  // Link Highlighting
-  if (pos2 > $('#home').offset().top)       { highlightLink('home'); }
-  if (pos2 > $('#profile').offset().top)      { highlightLink('profile'); }
-  if (pos2 > $('#portfolio').offset().top)  { highlightLink('portfolio'); }
-  if (pos2 > $('#contact').offset().top ||
-      pos + $(window).height() === $(document).height()) {
-        highlightLink('contact');
-  }
+//   // Link Highlighting
+//   if (pos2 > $('#home').offset().top)       { highlightLink('home'); }
+//   if (pos2 > $('#profile').offset().top)      { highlightLink('profile'); }
+//   if (pos2 > $('#portfolio').offset().top)  { highlightLink('portfolio'); }
+//   if (pos2 > $('#contact').offset().top ||
+//       pos + $(window).height() === $(document).height()) {
+//         highlightLink('contact');
+//   }
 
 
-  if(!$('body').hasClass('disable-hover')) {
-    $('body').addClass('disable-hover')
-  }
+//   if(!$('body').hasClass('disable-hover')) {
+//     $('body').addClass('disable-hover')
+//   }
 
-  lockTimer = setTimeout(function(){
-    $('body').removeClass('disable-hover')
-  }, 500);
-});
+//   lockTimer = setTimeout(function(){
+//     $('body').removeClass('disable-hover')
+//   }, 500);
+// });
 
-function highlightLink(anchor) {
-  $('nav .active').removeClass('active');
-  $("nav").find('[dest="' + anchor + '"]').addClass('active');
-}
+// function highlightLink(anchor) {
+//   $('nav .active').removeClass('active');
+//   $("nav").find('[dest="' + anchor + '"]').addClass('active');
+// }
