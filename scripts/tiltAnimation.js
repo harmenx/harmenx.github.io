7c50e3fc8d34d76bd91097a1383c610004cdbe4a
Harmen <harmendark@gmail.com>
2023-11-27 12:22:14 +0000

    files

--- scripts/tiltAnimation.js
+++
@@ -1,6 +0,0 @@
-import VanillaTilt from "./vanilla-tilt.min.js";
-
-export default function initTiltAnimation() {
-  const elements = document.querySelectorAll(".js-tilt");
-  VanillaTilt.init(elements);
-}
